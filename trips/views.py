5177b64a7e804fbbb78dcdc13951132b64b7dfe4
leo <leoyenschen@gmail.com>
2017-02-15 17:11:27 +0800

    first commit

---
+++ trips/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,render_to_response
+from django.template import RequestContext
+import sys
+import os
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+import json
+from time import time
+import random
+
+# Create your views here.
+def hello_world(request):
+    return render_to_response('hello_world.html',{},context_instance = RequestContext(request))
+
+
+@csrf_exempt
+def highchart_run(request):
+    print("leo test highchart_run")
+    #print(request.POST)
+    x = time() * 1000
+    y = random.randint(0,99)
+
+    return HttpResponse(json.dumps([x,y]), content_type="application/json")
+        #return render_to_response('strategy_run.html', locals())
